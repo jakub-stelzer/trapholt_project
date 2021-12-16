e49a801fcc2916ff44d7f5f5fdfdb301ea4ab0f4
Jakub Stelzer <jakub.stelzer1@gmail.com>
2021-12-16 20:01:49 +0100

    Final_one again
    Definitely!

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,9 @@
 var swiper = new Swiper(".mySwiper", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
